265d5891bf11da253d31fdf973c3787bd5481550
Prakhar Prakash Bhardwaj <prkhrbhardwaj6@gmail.com>
2020-02-28 17:07:59 +0530

    Inital Commit

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const store = require('./store')
+
+const app = express()
+const print = console.log
+
+app.use(express.static('public'))
+app.use(bodyParser.json())
+
+app.post('/createUser', (req, res) => {
+    store
+      .createUser({
+          mobileNo: req.body.mobileNo
+      })
+    .then(() => res.sendStatus(200))
+})
+app.listen(7555, () => {
+    print('Server running on http://localhost:7555')
+})
